4da3197a4c528a876194d194d72ddc8623aa3103
Luc4s L3e <32322377+leminhhoang1001@users.noreply.github.com>
2024-03-10 01:13:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.set("view engine", "ejs");
 
 //Modify your URL here
-var hostURL = "https://track-down-gamma.vercel.app/";
+var hostURL = "YOUR URL";
 //TOGGLE for Shorters
 var use1pt = false;
 
